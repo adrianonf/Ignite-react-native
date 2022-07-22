cdbf214099d0131190a21bd4bc21c13948db0020
Adriano <adriano.t.neto@hotmail.com>
2022-07-21 21:17:44 -0300

    feat: home/details/Register screen creation

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Heading, VStack, Icon, useTheme } from "native-base";
 import { Envelope, Key} from 'phosphor-react-native';
 import Logo from "../assets/logo_primary.svg";
@@ -5,25 +6,30 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function Signin() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     const { colors } = useTheme();
 
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
           <Heading color="gray.300" fontSize="xl" mt={20} mb={6}>
-             Acesse sua conta
+             Acesse sua conta 
           </Heading>
 
           <Input 
             mb={4}
             placeholder="E-mail"
             InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+            onChangeText={setName}
           />
           <Input 
             mb={8}
             placeholder="Senha"
             InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
             secureTextEntry
+            onChangeText={setPassword}
           />
           <Button title="Entrar" w="full"/>
         </VStack>
