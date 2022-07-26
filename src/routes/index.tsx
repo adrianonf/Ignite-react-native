ec61142870a8df6bfac9ca4fc894c0096b964a5d
Adriano <adriano.t.neto@hotmail.com>
2022-07-26 11:27:39 -0300

    feat: app finalizado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,32 @@
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 import { AppRoutes } from "./app.routes";
+import { Signin } from "../screens/Signin";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
-    return (
-        <NavigationContainer>
-            <AppRoutes />
-        </NavigationContainer>
-    )
+  const [loading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <AppRoutes /> : <Signin />}
+    </NavigationContainer>
+  );
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,39 +1,77 @@
 import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { Alert } from "react-native";
 import { Heading, VStack, Icon, useTheme } from "native-base";
-import { Envelope, Key} from 'phosphor-react-native';
+import { Envelope, Key } from "phosphor-react-native";
 import Logo from "../assets/logo_primary.svg";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function Signin() {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const { colors } = useTheme();
+  const { colors } = useTheme();
 
-    return(
-        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-            <Logo />
-          <Heading color="gray.300" fontSize="xl" mt={20} mb={6}>
-             Acesse sua conta 
-          </Heading>
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
 
-          <Input 
-            mb={4}
-            placeholder="E-mail"
-            InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-            onChangeText={setName}
-          />
-          <Input 
-            mb={8}
-            placeholder="Senha"
-            InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-            secureTextEntry
-            onChangeText={setPassword}
-          />
-          <Button title="Entrar" w="full"/>
-        </VStack>
-    )
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido.")
+        }
+
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "Usuário não cadastrado.")
+        }
+
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.")
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar.")
+
+      });
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+      <Heading color="gray.300" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
+    </VStack>
+  );
 }
-
-
